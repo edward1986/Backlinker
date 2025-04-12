e0a9fbe52494e47868e3a95ea5362da55594e574
Edward Lance Arellano Lorilla <edwardlorilla1986@gmail.com>
2025-04-12 11:50:08 +0800

    Update backlinker.py

--- backlinker.py
+++ backlinker.py
@@ -1,29 +1,48 @@
-import json,requests,re,sys
+import json
+import requests
+import re
+import sys
 
-try:
-  print("""
+def print_banner():
+    print(r"""
             _ ____             _    _ _       _             
            | |  _ \           | |  | (_)     | |            
  _   _ _ __| | |_) | __ _  ___| | _| |_ _ __ | | _____ _ __ 
 | | | | '__| |  _ < / _` |/ __| |/ / | | '_ \| |/ / _ \ '__|
 | |_| | |  | | |_) | (_| | (__|   <| | | | | |   <  __/ |   
  \__,_|_|  |_|____/ \__,_|\___|_|\_\_|_|_| |_|_|\_\___|_|   
-                                              H4-cklinker - wmdark.com     
-  """)
-  if (sys.version_info.major == 3):
-    site = input(" => Backlink Kasilcak Site\t: ")
-  else:
-    site = raw_input(" => Backlink Kasilcak Site\t: ")
-  with open("urlbacklinks.json", "r") as file:
-    data = json.loads(file.read())
-    for backlink in data:
-      url = backlink['url'].replace("h4link.duckdns.org", site)
-      try:
-        r = requests.get(url).status_code
-      except KeyboardInterrupt:
-        sys.exit()
-      except:
-        r = "time out"
-      print(site + " => Backlink Eklendi ==> "+re.search('http:\/\/.*?\/', url).group(0).replace("/", "").replace("http:","") + " status: "+str(r))
-except:
-  print("\n\n => exit\n")
+
+                                             H4-cklinker - wmdark.com
+    """)
+
+def main():
+    try:
+        print_banner()
+
+        # Use your desired site automatically
+        site = "multiculturaltoolbox.com"
+
+        with open("urlbacklinks.json", "r") as file:
+            data = json.load(file)
+
+            for backlink in data:
+                url = backlink['url'].replace("h4link.duckdns.org", site)
+
+                try:
+                    response = requests.get(url, timeout=5)  # Added timeout for safety
+                    status = response.status_code
+                except requests.RequestException as e:
+                    status = "error"
+
+                try:
+                    domain = re.search(r'http://.*?/', url).group(0).replace("/", "").replace("http:", "")
+                except AttributeError:
+                    domain = "invalid-url"
+
+                print(f"{site} => Backlink Eklendi ==> {domain} status: {status}")
+
+    except Exception as e:
+        print(f"\n\n => exit due to error: {e}\n")
+
+if __name__ == "__main__":
+    main()
